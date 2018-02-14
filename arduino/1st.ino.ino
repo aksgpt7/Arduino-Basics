be739b15ec1bcf9b0f2aa3be55cafc78fb657d0e
AKSHAT GUPTA <33572137+aksgpt7@users.noreply.github.com>
2018-02-14 20:41:33 +0530

    Add files via upload

---
+++ arduino/1st.ino.ino
@@ -0,0 +1,27 @@
+int a=A1;
+int x;
+void setup() {
+  pinMode(a,INPUT) ;
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+  x=analogRead(a);
+ 
+  Serial.println(x);
+ 
+  //Serial.println(x);
+//Serial.print("Y reading:");
+ // Serial.print(y);
+//Serial.print("Z reading: ");
+//  Serial.print(z);
+ delay(100);
+  
+  // put your main code here, to run repeatedly:
+
+  
+}
---
+++ arduino/accelerometer.ino.ino
@@ -0,0 +1,36 @@
+int a = A1;
+int x, y,z;
+int b = A2 ;
+
+int c = A3 ;
+void setup() {
+  pinMode(a, INPUT) ;
+  pinMode(b, INPUT);
+ pinMode(c, INPUT) ;
+  Serial.begin(9600);
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  x = analogRead(a);
+  y = analogRead(b);
+  z = analogRead(c);
+  Serial.print(" X reading:\t");
+  Serial.println(x);
+  // delay(1000) ;
+  Serial.print("Y reading: \t");
+  Serial.println(y);
+  //delay(1000) ;
+  Serial.print("Z reading: \t ");
+  Serial.println(z);
+   delay(1000) ;
+
+  
+
+
+  // put your main code here, to run repeatedly:
+
+
+}
